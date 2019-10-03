c19ec6044da233de77f1afc4575c0e75ffc9e438
Vivian <vivz753@gmail.com>
2019-10-03 00:42:24 -0700

    clean up

--- todoist-tutorial/src/components/AddTask.js
+++ todoist-tutorial/src/components/AddTask.js
@@ -34,9 +34,7 @@ export const AddTask = ({
     else if (projectId === 'NEXT_7') {
       collatedDate = moment().add(7, 'days').format('DD/MM/YYYY')
     }
-
-    console.log('collatedDate: ' + collatedDate)
-    console.log('taskDate: ' + taskDate)
+    
     return (
       taskName && 
       projectId && 
