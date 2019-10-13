d0cfe0db06e07631bb9a83954f19ab5e9a7ddc7f
Vivian <vivz753@gmail.com>
2019-10-12 17:23:04 -0700

    show/hide projects when clicked at sidebar

--- todoist-tutorial/src/components/layout/Sidebar.js
+++ todoist-tutorial/src/components/layout/Sidebar.js
@@ -1,17 +1,11 @@
 import React, { useState } from 'react';
-import { 
-  FaChevronDown, 
-  FaInbox, 
-  FaRegCalendarAlt, 
-  FaRegCalendar } from 'react-icons/fa';
+import { FaChevronDown } from 'react-icons/fa';
 import { useSelectedProjectValue } from '../../context/index';
 import { Projects } from '../Projects';
 import { CollatedProjects } from '../CollatedProjects';
 import { AddProject } from '../AddProject';
 
 export const Sidebar = () => {
-  // const { setSelectedProject } = useSelectedProjectValue();
-  // const [ active, setActive ] = useState('INBOX');
   const [ showProjects, setShowProjects ] = useState(true)
 
   return (
@@ -19,8 +13,17 @@ export const Sidebar = () => {
       <div className="sidebar__generic">
         <CollatedProjects />
       </div>
-      <div className="sidebar__middle">
-        <span><FaChevronDown /></span>
+      <div 
+        aria-label="Show/hide projects"
+        className="sidebar__middle"
+        onClick={() => setShowProjects(!showProjects)}
+        onKeyDown={() => setShowProjects(!showProjects)}
+      >
+        <span>
+          <FaChevronDown
+            className={showProjects ? undefined : 'hidden-projects'}
+          />
+        </span>
         <h2>Projects</h2>
       </div>
       <ul className="sidebar__projects">
